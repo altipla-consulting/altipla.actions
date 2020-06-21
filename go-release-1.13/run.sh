f5583ba20c0b4fe66d04882337bdcb46d53d712e
Ernesto Alejo <ernestokarim@gmail.com>
2020-06-21 23:59:54 +0200

    Fix name of the deployed binary when the folder is the current pwd.

--- go-release-1.13/run.sh
+++ go-release-1.13/run.sh
@@ -2,8 +2,10 @@
 
 set -eu
 
+CMD=${COMMAND:-$(pwd)}
+
 cd $GITHUB_WORKSPACE
-go install -v $COMMAND
+go install -v $CMD
 
 EVENT_DATA=$(cat $GITHUB_EVENT_PATH)
 UPLOAD_URL=$(echo $EVENT_DATA | jq -r .release.upload_url)
@@ -14,7 +16,7 @@ NAME="${PROJECT_NAME}_${RELEASE_NAME}_linux_amd64"
 
 curl \
   -X POST \
-  --data-binary @/go/bin/$(basename $COMMAND) \
+  --data-binary @/go/bin/$(basename $CMD) \
   -H 'Content-Type: application/gzip' \
   -H "Authorization: Bearer ${GITHUB_TOKEN}" \
   "${UPLOAD_URL}?name=${NAME}"
